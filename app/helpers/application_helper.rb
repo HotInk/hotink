89b8bf981eda14c9029ab4a6c701c9e864c26a1a
Christopher Dinn <me@christopher-dinns-macbook-pro.local>
2009-02-10 22:55:30 -0500

    Created button link helper for submit buttons and adjusted category form to use it.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -50,5 +50,17 @@ module ApplicationHelper
    def button_link_to_remote(name, options = {}, html_options = nil)
        button_link_to_function(name, remote_function(options), html_options || options.delete(:html))
    end
+   
+   def button_link_to_submit(name, *args, &block)
+       html_options = args.extract_options!.symbolize_keys
+
+       html_options.default = ""
+       html_options[:class] = html_options[:class] + " btn"
+
+       function = block_given? ? update_page(&block) : args[0] || ''
+       href = html_options[:href] || '#'
+
+       content_tag(:button,"<span><span>" + name + "</span></span>", html_options.merge(:href => href, :type => "submit"))
+   end
   
 end
