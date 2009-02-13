56ef5a89c84947487a8160c1a46fb234c4010693
Andrew Louis <andrew@hyfen.net>
2009-02-13 11:33:21 -0500

    tweaked fixtures for accounts, articles, authors and did some basic unit tests for article and accounts

--- test/unit/account_test.rb
+++ test/unit/account_test.rb
@@ -1,8 +1,22 @@
 require 'test_helper'
 
 class AccountTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  
+  def test_requires_name    
+    a = Account.new
+    a.save
+    assert_not_nil a.errors[:name]
+    a.name = "test"
+    a.save
+    assert a.errors.empty?
   end
+  
+  def test_account_must_be_unique
+    a = Account.new
+    a.name = "varsity" #fixture already has a varsity account
+    a.save
+    assert_not_nil a.errors[:name]
+  end
+  
+
 end
--- test/unit/article_test.rb
+++ test/unit/article_test.rb
@@ -1,8 +1,13 @@
 require 'test_helper'
 
 class ArticleTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+
+  def test_display_title
+    assert_equal articles(:one).display_title, "Simon Fraser union battle"
   end
+
+  def test_no_title_display_title
+    assert_equal articles(:two).display_title, "(no headline)"
+  end
+  
 end
