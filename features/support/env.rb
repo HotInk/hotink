9c637d942db8f915c76d7d62ba81818ff0bb43b5
Chris Dinn <chris@massaudience.ca>
2009-03-16 13:28:19 -0400

    Re run of coverage with culerity rather than webrat. Final testing stack seems to be in place.

--- features/support/env.rb
+++ features/support/env.rb
@@ -5,15 +5,9 @@ require 'cucumber/rails/world'
 require 'cucumber/formatters/unicode' # Comment out this line if you don't want Cucumber Unicode support
 Cucumber::Rails.use_transactional_fixtures
 
-require 'webrat'
 require 'cucumber/rails/rspec'
-require 'webrat/core/matchers'
 
 require 'factory_girl' 
 require 'test/factories'
 
-
-Webrat.configure do |config|
-  config.mode = :rails
-end
 
