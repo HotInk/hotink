7c2394e315d1656b7a8e110c035860d3c28f20bc
Christopher Dinn <me@christopher-dinns-macbook-pro.local>
2009-04-01 18:10:07 -0400

    Added a little colour to the articles index page.

--- test/factories.rb
+++ test/factories.rb
@@ -2,3 +2,8 @@ Factory.define :account do |a|
   a.name "onlinejournalgazette"
   a.time_zone "Eastern Time (US & Canada)"
 end
+
+Factory.define :article do |a|
+  a.date Time.now
+  a.account {|a| a.association(:account) }
+end
