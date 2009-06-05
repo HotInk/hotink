d124e628f4c127d7f67eb0a4e7e934aa110c0792
Christopher Dinn <me@christopher-dinns-macbook-pro.local>
2009-06-04 22:34:06 -0400

    Fixed time format and reorganized article card.

--- config/initializers/time_formats.rb
+++ config/initializers/time_formats.rb
@@ -1,30 +1,30 @@
 Time::DATE_FORMATS[:time] = "%l:%M %p"
 
 Time::DATE_FORMATS[:standard] = lambda do |date|
-    if Time.now <= date
-      Time.now.end_of_day >= date ? "#{date.strftime('%l').to_i}:#{date.strftime('%M')} #{date.strftime('%p').downcase}" : 
-      Time.now.end_of_year >= date ? "#{date.strftime('%b')} #{date.day}" : "#{date.month}/#{date.day}/#{date.strftime('%y')}"
+    if Time.zone.now <= date
+      Time.zone.now.end_of_day >= date ? "#{date.strftime('%l').to_i}:#{date.strftime('%M')} #{date.strftime('%p').downcase}" : 
+      Time.zone.now.end_of_year >= date ? "#{date.strftime('%b')} #{date.day}" : "#{date.month}/#{date.day}/#{date.strftime('%y')}"
     else
-      Time.now.beginning_of_day <= date ? "#{date.strftime('%l').to_i}:#{date.strftime('%M')} #{date.strftime('%p').downcase}" : 
-      Time.now.beginning_of_year <= date ? "#{date.strftime('%b')} #{date.day}" : "#{date.month}/#{date.day}/#{date.strftime('%y')}"
+      Time.zone.now.beginning_of_day <= date ? "#{date.strftime('%l').to_i}:#{date.strftime('%M')} #{date.strftime('%p').downcase}" : 
+      Time.zone.now.beginning_of_year <= date ? "#{date.strftime('%b')} #{date.day}" : "#{date.month}/#{date.day}/#{date.strftime('%y')}"
     end
 end
 
 
 ActiveSupport::CoreExtensions::Time::Conversions::DATE_FORMATS.merge!(
   :standard_date_only => lambda do |date| 
-    if Time.now <= date
-      Time.now.end_of_year >= date ? "#{date.strftime('%b')} #{date.day}" : "#{date.month}/#{date.day}/#{date.strftime('%y')}"
+    if Time.zone.now <= date
+      Time.zone.now.end_of_year >= date ? "#{date.strftime('%b')} #{date.day}" : "#{date.month}/#{date.day}/#{date.strftime('%y')}"
     else
-      Time.now.beginning_of_year <= date ? "#{date.strftime('%b')} #{date.day}" : "#{date.month}/#{date.day}/#{date.strftime('%y')}"
+      Time.zone.now.beginning_of_year <= date ? "#{date.strftime('%b')} #{date.day}" : "#{date.month}/#{date.day}/#{date.strftime('%y')}"
     end
   end
 )
 
 ActiveSupport::CoreExtensions::Date::Conversions::DATE_FORMATS.merge!(
-  :standard => lambda { |date| Time.now.beginning_of_day <= date ? 
+  :standard => lambda { |date| Time.zone.now.beginning_of_day <= date ? 
     "#{date.strftime('%l').to_i}:#{date.strftime('%M')} #{date.strftime('%p').downcase}" : 
-    (Time.now.beginning_of_year <= date ? "#{date.strftime('%b')} #{date.day}" : 
+    (Time.zone.now.beginning_of_year <= date ? "#{date.strftime('%b')} #{date.day}" : 
     "#{date.month}/#{date.day}/#{date.strftime('%y')}") 
   }
 )
