d39aa6cb1ddbdaa0e105f2d17c19e1fa6c618a6f
Chris Dinn <chris@massaudience.ca>
2009-06-23 11:30:54 -0400

    Updates to article form presentation and addition of article show action.

--- config/environment.rb
+++ config/environment.rb
@@ -54,6 +54,7 @@ Rails::Initializer.run do |config|
   config.gem "mislav-will_paginate", :lib=>"will_paginate", :source => "http://gems.github.com"
   config.gem 'cucumber'
   
+  config.gem 'BlueCloth'
   
   # Only load the plugins named here, in the order given. By default, all plugins 
   # in vendor/plugins are loaded in alphabetical order.
