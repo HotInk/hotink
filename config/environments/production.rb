83d63489f4d9831b55f1574188f55923ab3dcc56
Chris Dinn <chris@massaudience.ca>
2009-07-14 13:27:30 -0400

    Change SMTP server for theorem.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,6 +24,6 @@ config.action_controller.perform_caching             = true
 # config.action_mailer.raise_delivery_errors = false
 
 ActionMailer::Base.smtp_settings = {
-  :address => 'outbox.allstream.net',
-  :domain => 'campusplus.com'
+  :address => 'theorem.ca',
+  :domain => 'theorem.ca'
 }
