36950caacf409d21e5e730b5bb3c2c202e3cca57
Chris Dinn <chris@massaudience.ca>
2009-07-14 11:11:19 -0400

    Updated bluecloth gem dependency.

--- config/environment.rb
+++ config/environment.rb
@@ -53,7 +53,7 @@ Rails::Initializer.run do |config|
   config.gem "bcrypt-ruby", :lib => "bcrypt"
   config.gem "mislav-will_paginate", :lib=>"will_paginate", :source => "http://gems.github.com"
   
-  config.gem 'BlueCloth'
+  config.gem 'bluecloth'
   
   # Only load the plugins named here, in the order given. By default, all plugins 
   # in vendor/plugins are loaded in alphabetical order.
