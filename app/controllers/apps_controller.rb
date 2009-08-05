565facf3384ea3421c0523ac34321f4eaa16d5aa
Chris Dinn <chris@massaudience.ca>
2009-08-05 15:34:21 -0400

    Added infrastructure to pass along requested data to applications.

--- app/controllers/apps_controller.rb
+++ app/controllers/apps_controller.rb
@@ -4,6 +4,13 @@ class AppsController < ApplicationController
   
   def show
     @app = ClientApplication.find(params[:id])
+    query_string = Hash.new
+    query_string.merge!(:account_id => @account.id) if @account && @app.include=="account_id"
+    if @app.include
+       @iframe_url = "#{@app.callback_url}?#{query_string.to_query}"
+     else
+       @iframe_url = @app.callback_url
+    end
   end
   
 end
