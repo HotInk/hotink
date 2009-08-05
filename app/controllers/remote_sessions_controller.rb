52e5d739e4bf06ee4c40ea7c45bc0dd08942dd4e
Christopher Dinn <me@christopher-dinns-macbook-pro.local>
2009-08-05 18:45:39 -0400

    Remote Sessions controller now preserves passed in request_url parameter for requesting application.

--- app/controllers/remote_sessions_controller.rb
+++ app/controllers/remote_sessions_controller.rb
@@ -1,15 +1,18 @@
 class RemoteSessionsController < ApplicationController
 
   def new
-   
     @app = ClientApplication.find_by_key(params[:key], :order=>'created_at DESC')
     @access_token = AccessToken.find_by_user_id_and_client_application_id(current_user.id, @app.id)
     
     if @access_token
       # If an access token is fine, send everything necessary back to the app, but sign the request for security.
-      redirect_to(@app.callback_url + "?oauth_token=#{@access_token.token}&sig=#{Digest::SHA1.hexdigest(@access_token.token + @access_token.secret)}")
+      query_string = "?oauth_token=#{@access_token.token}&sig=#{Digest::SHA1.hexdigest(@access_token.token + @access_token.secret)}"
+      query_string += "&request_url=#{params[:request_url]}" if params[:request_url]
+      redirect_to(@app.callback_url + query_string )
     else
-      redirect_to(@app.callback_url + "?session_action=new_user")
+      query_string = "?session_action=new_user"
+      query_string += "&request_url=#{params[:request_url]}" if params[:request_url]
+      redirect_to(@app.callback_url + query_string)
       return
     end  
          
