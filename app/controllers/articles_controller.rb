aefd413d81f67f98b4678aa7c9f59227f403d9a8
Chris Dinn <chris@massaudience.ca>
2009-08-22 11:33:27 -0400

    Better article search style.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -27,8 +27,7 @@ class ArticlesController < ApplicationController
         @drafts = @account.articles.find( :all, :conditions => { :status => nil }, :include => [:authors, :mediafiles, :section] ).reject{ |draft| draft.created_at == draft.updated_at } unless params[:page]
       else  
         @search_query = params[:search]
-        @search_results = @account.articles.search( @search_query, :page=>(params[:page] || 1), :per_page => (params[:per_page] || 20 ), :include => [:authors, :mediafiles, :section])
-        @articles = @search_results.collect
+        @articles = @account.articles.search( @search_query, :page=>(params[:page] || 1), :per_page => (params[:per_page] || 20 ), :include => [:authors, :mediafiles, :section])
       end
     
       respond_to do |format|
