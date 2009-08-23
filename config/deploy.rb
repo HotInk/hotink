79204feabec4b0d9717fe18cd319c4e127db735e
Christopher Dinn <me@christopher-dinns-macbook-pro.local>
2009-08-23 00:41:38 -0400

    Take two deploy.rb.

--- config/deploy.rb
+++ config/deploy.rb
@@ -97,8 +97,14 @@ after :deploy, "passenger:restart"
 
 after "deploy:setup", "thinking_sphinx:shared_sphinx_folder"
 
+task :before_update_code, :roles => [:app] do
+  thinking_sphinx.stop
+end
+
 task :after_update_code, :roles => [:app] do
   symlink_sphinx_indexes
+  thinking_sphinx.configure
+  thinking_sphinx.start
 end
 
 task :symlink_sphinx_indexes, :roles => [:app] do
