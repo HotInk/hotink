28482b1dd6f6acffeae1653fdaee8ef6c4dd85c7
Christopher Dinn <me@christopher-dinns-macbook-pro.local>
2009-09-29 00:48:41 -0400

    Switch delayed_job deploy format.

--- config/deploy.rb
+++ config/deploy.rb
@@ -101,17 +101,17 @@ after "deploy:setup", "thinking_sphinx:shared_sphinx_folder"
 namespace :delayed_job do
   desc "Start delayed_job process" 
   task :start, :roles => :app do
-    run "cd #{current_path}; script/delayed_job start #{rails_env}" 
+    run "cd #{current_path}; RAILS_ENV=production script/delayed_job start" 
   end
 
   desc "Stop delayed_job process" 
   task :stop, :roles => :app do
-    run "cd #{current_path}; script/delayed_job stop #{rails_env}" 
+    run "cd #{current_path}; RAILS_ENV=production script/delayed_job stop" 
   end
 
   desc "Restart delayed_job process" 
   task :restart, :roles => :app do
-    run "cd #{current_path}; script/delayed_job restart #{rails_env}" 
+    run "cd #{current_path}; RAILS_ENV=production script/delayed_job restart" 
   end
 end
 
