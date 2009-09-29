f1fc8d349f68db1d893b54cb2835b765a1ce50cd
Christopher Dinn <me@christopher-dinns-macbook-pro.local>
2009-09-29 00:44:59 -0400

    Add delayed_job deploy tasks.

--- config/deploy.rb
+++ config/deploy.rb
@@ -97,3 +97,24 @@ end
 after :deploy, "passenger:restart"
 
 after "deploy:setup", "thinking_sphinx:shared_sphinx_folder"
+
+namespace :delayed_job do
+  desc "Start delayed_job process" 
+  task :start, :roles => :app do
+    run "cd #{current_path}; script/delayed_job start #{rails_env}" 
+  end
+
+  desc "Stop delayed_job process" 
+  task :stop, :roles => :app do
+    run "cd #{current_path}; script/delayed_job stop #{rails_env}" 
+  end
+
+  desc "Restart delayed_job process" 
+  task :restart, :roles => :app do
+    run "cd #{current_path}; script/delayed_job restart #{rails_env}" 
+  end
+end
+
+after "deploy:start", "delayed_job:start" 
+after "deploy:stop", "delayed_job:stop" 
+after "deploy:restart", "delayed_job:restart"
