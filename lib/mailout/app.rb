d69bc3b657c1cebba9138aa57e2d7ed1930e86ac
Chris Dinn <chris@massaudience.ca>
2009-12-16 15:58:48 -0500

    Added some specs/structure to mailout app.

--- lib/mailout/app.rb
+++ lib/mailout/app.rb
@@ -9,6 +9,8 @@ module Mailout
     API_KEY = 'e03757750894d3afb19d93edf0bf9421-us1'
     
     def initialize_mailchimp
+      @account = Account.find(params[:id])
+      halt 404 unless @account
       @mailchimp = Hominid::Base.new({:api_key => API_KEY })
     end
 
@@ -16,6 +18,17 @@ module Mailout
       initialize_mailchimp
       @campaigns = @mailchimp.campaigns
       erb :mailouts
+    end
+    
+    get '/accounts/:id/mailouts/new' do
+      initialize_mailchimp
+      erb :new_mailout
+    end
+    
+    get '/accounts/:id/mailouts/:mailout' do
+      initialize_mailchimp
+      @campaign = @mailchimp.find_campaign_by_id(params[:mailout])
+      erb :mailout
     end
 
   end
--- spec/metals/mailout_spec.rb
+++ spec/metals/mailout_spec.rb
@@ -16,22 +16,35 @@ describe Mailout do
   
   describe "GET to /accounts/:id/mailouts" do
     it "should display all campaigns from Mailchimp" do
-      @mailer.should_receive(:campaigns)
+      @mailer.should_receive(:campaigns).and_return(Array.new)
       get "/accounts/#{@account.id}/mailouts"
       last_response.should be_ok
     end
   end
   
   describe "GET to /accounts/:id/mailouts/new" do
-    it "should display the mailout form"
+    it "should display the mailout form" do
+      get "/accounts/#{@account.id}/mailouts/new"
+      last_response.should be_ok
+      last_response.body.should have_selector("form[action=\"/accounts/#{@account.id}/mailouts\"]")
+    end
   end
   
-  describe "GET to /accounts/:id/mailouts" do
+  describe "POST to /accounts/:id/mailouts" do
     it "should create an unsent mailout"
   end
   
   describe "GET to /accounts/:id/mailouts/:mailout" do
-    it "should display a preview of the mailout"
+    before do
+      @campaign = mock("campaign")
+      @campaign.stub!(:[]).and_return("stubbed_info")
+      @mailer.should_receive(:find_campaign_by_id).with("sample_id").and_return(@campaign)
+      get "/accounts/#{@account.id}/mailouts/sample_id"
+    end
+    
+    it "should display a preview of the mailout" do
+      last_response.body.should have_selector("iframe[src=\"#{@campaign['archive_url']}\"]")
+    end
     it "should display a send button for unsent mailout"
   end
   
