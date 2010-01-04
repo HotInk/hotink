99bb9be403f9c6f6474b9d8e09a3ed66bcb4c1f1
Chris Dinn <chris@massaudience.ca>
2010-01-04 13:50:10 -0500

    Entry controller 'status' handling now matches articles controller.

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -71,17 +71,23 @@ class EntriesController < ApplicationController
   
   def update
     @entry = @blog.entries.find(params[:id])
-    
+      
     # Only touch published status if status is passed
-    if params[:entry][:status]      
-      # Should we schedule publishing on a custom date or immediately?
-      # Hot Ink relies on a "schedule" parameter to determine which.
-      if params[:entry][:schedule] 
-        schedule = params[:entry].delete(:schedule)
-        @entry.publish(params[:entry].delete(:status), Time.local(schedule[:year].to_i, schedule[:month].to_i, schedule[:day].to_i, schedule[:hour].to_i, schedule[:minute].to_i) )
-      else
-        @entry.publish(params[:entry].delete(:status))
+    if params[:entry][:status]=="Published"
+      
+      if permit?("(manager of account) or (editor of blog) or (contributor to blog) or admin")
+        # Should we schedule publishing on a custom date or immediately?
+        # Rely on a "schedule" parameter to determine which.
+        if params[:entry][:schedule] 
+          schedule = params[:entry].delete(:schedule)
+          @entry.schedule(Time.local(schedule[:year].to_i, schedule[:month].to_i, schedule[:day].to_i, schedule[:hour].to_i, schedule[:minute].to_i))
+        else
+          @entry.publish
+        end
       end
+
+    elsif params[:entry][:status]==""
+      params[:entry][:status]=nil #To make sure an entry is upublished properly
     end
   
     respond_to do |format|
