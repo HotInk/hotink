4684909b176c4aa8edfd07bacbc3d991e99c99a3
chrisdinn <chrisgdinn@gmail.com>
2010-01-29 09:50:35 -0500

    Prevent user model from failing on NULL values

--- db/migrate/20090407172507_create_users.rb
+++ db/migrate/20090407172507_create_users.rb
@@ -1,13 +1,13 @@
 class CreateUsers < ActiveRecord::Migration
   def self.up
     create_table :users, :options => 'DEFAULT CHARSET=utf8' do |t|
-      t.string    :login,               :null => false                
-      t.string    :email,               :null => false
-      t.string    :crypted_password,    :null => false                
-      t.string    :password_salt,       :null => false                
-      t.string    :persistence_token,   :null => false                
-      t.string    :single_access_token, :null => false                # see Authlogic::Session::Params
-      t.string    :perishable_token,    :null => false                # see Authlogic::Session::Perishability
+      t.string    :login,               :null => false, :default => ""                
+      t.string    :email,               :null => false, :default => ""
+      t.string    :crypted_password,    :null => false, :default => ""                
+      t.string    :password_salt,       :null => false, :default => ""                
+      t.string    :persistence_token,   :null => false, :default => ""                
+      t.string    :single_access_token, :null => false, :default => ""# see Authlogic::Session::Params
+      t.string    :perishable_token,    :null => false, :default => ""# see Authlogic::Session::Perishability
       t.integer   :login_count,         :null => false, :default => 0 # see Authlogic::Session::MagicColumns
       t.integer   :failed_login_count,  :null => false, :default => 0 # see Authlogic::Session::MagicColumns
       t.datetime  :last_request_at                                    # see Authlogic::Session::MagicColumns
