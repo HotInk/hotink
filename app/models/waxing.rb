d2ddac49c4b3b5412a67bfb5e83d5d3059121594
Chris Dinn <chrisgdinn@gmail.com>
2010-08-07 17:13:01 -0400

    Upgrade waxing edit form.

--- app/models/waxing.rb
+++ app/models/waxing.rb
@@ -3,8 +3,10 @@ class Waxing < ActiveRecord::Base
   
   belongs_to :mediafile
   validates_presence_of :mediafile
+  accepts_nested_attributes_for :mediafile, :update_only => true, :reject_if => :all_blank
   
   belongs_to :document
   validates_presence_of :document
   belongs_to :article, :class_name => "Article", :foreign_key => "document_id"
+  
 end
