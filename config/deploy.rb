38515a013e444e0b14397b3d41f76c6acbd046f7
Chris Dinn <chrisgdinn@gmail.com>
2010-08-09 19:17:06 -0400

    Some things never made it into the repository it seems.

--- config/deploy.rb
+++ config/deploy.rb
@@ -72,6 +72,24 @@ production:
 end
 
 #############################
+# bundler recipes
+
+namespace :bundler do
+  task :create_symlink, :roles => :app do
+    shared_dir = File.join(shared_path, 'bundle')
+    release_dir = File.join(current_release, '.bundle')
+    run("mkdir -p #{shared_dir} && ln -s #{shared_dir} #{release_dir}")
+  end
+  
+  task :bundle_new_release, :roles => :app do
+    bundler.create_symlink
+    run "cd #{release_path} && bundle install --without test"
+  end
+end
+
+after 'deploy:update_code', 'bundler:bundle_new_release'
+
+#############################
 # passenger restart recipes
 
 namespace :deploy do
