bc0ea49d1ef94d3f7e8f2c6dbfb2b29bdb1a94d9
Chris Dinn <chrisgdinn@gmail.com>
2010-08-25 23:02:51 -0400

    public controller was preventing account not found from propogating.

--- app/controllers/public_controller.rb
+++ app/controllers/public_controller.rb
@@ -2,6 +2,7 @@ class PublicController < ApplicationController
   skip_before_filter :login_required
   
   rescue_from ActiveRecord::RecordNotFound do |exception|
+    raise ActiveRecord::RecordNotFound unless @account
     if @design = design_to_render
       rendered_page =  @design.not_found_template.render({'content' => ContentDrop.new(@account), 'site' => SiteDrop.new}, :registers => { :design => @design })
       render :text => rendered_page, :status => :not_found
