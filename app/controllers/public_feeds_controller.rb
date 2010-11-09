304406d6684ad21345529118b5d626614eea7560
Chris Dinn <chrisgdinn@gmail.com>
2010-11-08 23:11:43 -0500

    Add RSS feed settings, to control full-text/truncated and article limit.

--- app/controllers/public_feeds_controller.rb
+++ app/controllers/public_feeds_controller.rb
@@ -2,7 +2,9 @@ class PublicFeedsController < PublicController
   
   def show
     @feed_title = "#{@account.display_name} RSS feed"
-    @articles = @account.articles.published.find(:all, :order =>"published_at desc", :limit => 250)
+    @articles = @account.articles.published.find  :all, 
+                                                  :order =>"published_at desc", 
+                                                  :limit => @account.feed_settings.limit
     respond_to do |format|
       format.xml
     end
